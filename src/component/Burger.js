4e0282c5c3a608077eee6f8c465316ab1a2075fe
Muhammad Umair Zahid <umair.zahid@devsinc.com>
2022-09-07 10:36:25 +0500

    first commit

---
+++ src/component/Burger.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Burger.css';
+
+export default class Burger extends Component{
+  state ={
+    luttece: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  }
+
+  render(){
+    return(
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
